0c715b05f6aa35b822a5a052f70e889ec5b9a761
Angel Irangabiye <angel.irangabiyr@stu.ocsb.ca>
2025-02-20 08:39:27 +0000

    Added areaPerRect.cpp

---
+++ areaPerRect.cpp
@@ -0,0 +1,16 @@
+// Copyright (c) 2020 Angel All rights reserved
+//
+// Created by: Angel
+// Created on Feb 18,2025
+// This program calculates the area and perimeter of a rectangle
+//   with dimensions 5cm x 3cm
+
+#include <iostream>
+
+int main(){
+    std::cout << "If a rectangle has the dimensions:" << std::endl;
+    std::cout << "5cm x 3cm" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "Area is: " << "cm^2" << std::end;
+    std::cout << "Perimeter is: " << (2*(5+3)) << "cm" << std::endl;
+}
